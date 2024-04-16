79106c7a0dbbad4df7070d3e87e21762596bbc40
Sangyoon Kim <sangyun0904@gmail.com>
2024-04-16 14:39:19 +0900

    cargo new hello_cargo cargo rust 프로젝트 생성

---
+++ hello_cargo/target/debug/deps/hello_cargo-d91335f21db80615.d
@@ -0,0 +1,5 @@
+C:\Users\user\sykim\sangyun0904\rust\hello_cargo\target\debug\deps\libhello_cargo-d91335f21db80615.rmeta: src\main.rs
+
+C:\Users\user\sykim\sangyun0904\rust\hello_cargo\target\debug\deps\hello_cargo-d91335f21db80615.d: src\main.rs
+
+src\main.rs:
