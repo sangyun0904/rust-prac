627e7a4c5ba5a174bde29108e0fb3983b0559882
Sangyoon Kim <sangyun0904@gmail.com>
2024-04-16 14:46:11 +0900

    cargo build cargo 프로젝트 빌드 파일 생성

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\user\sykim\sangyun0904\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\user\sykim\sangyun0904\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
